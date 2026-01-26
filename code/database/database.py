135ce592d1b32d48d2722c93966c3f7497531b57
Joel Wang <joelwangusa@gmail.com>
2026-01-26 14:16:19 -0800

    new version of code after refactoring

--- code/database/database.py
+++ code/database/database.py
@@ -1,43 +1,45 @@
 import time
 
 class Database:
-    """in-memory database from scratch"""
-    
-    def __init__(self) -> None:
+    def __init__(self):
         self._data = {}
         self._expire = {}
-
+    
     def set(self, key: str, value: str, ttl: int | None = None) -> None:
         self._data[key] = value
+        
         if ttl is not None:
             self._expire[key] = time.time() + ttl
         elif key in self._expire:
+            # edge case, when reset the key without ttl, need to clean existing ttl
             del self._expire[key]
     
     def get(self, key: str) -> str | None:
+        # if expired, should return None
         return self._data[key] if key in self._data and not self._is_expired(key) else None
     
     def delete(self, key: str) -> bool:
+        """return True if key existed"""
         if key not in self._data:
             return False
-        
-        del self._data[key]
-        
-        is_expired = False
-        if key in self._expire:
-            is_expired = self._is_expired(key)
-            del self._expire[key]
 
-        return not is_expired
+        del self._data[key]
+
+        # Return False on expired key
+        delete_status = True
+        if key in self._expire and self._is_expired(key):
+            delete_status = False
+        
+        # Remove the key from self._expire after expiration check
+        self._expire.pop(key, None)
+        
+        return delete_status
+    
+    def scan(self) -> list[tuple[str, str]]:
+        return [(k, v) for k, v in sorted(self._data.items(), key=lambda x:x[0]) if not self._is_expired(k)]
+    
+    def scan_by_prefix(self, prefix: str) -> list[tuple[str, str]]:
+        return [(k, v) for k, v in sorted(self._data.items(), key=lambda x:x[0]) if k.startswith(prefix) and not self._is_expired(k)]
     
     def _is_expired(self, key: str) -> bool:
         return key in self._expire and self._expire[key] < time.time()
-    
-    def scan(self) -> list[tuple[str, str]]:
-        """Return key-value pairs, sorted by key"""
-        return [(k, v) for k, v in sorted(self._data.items(), key=lambda x:x[0]) if not self._is_expired(k)]
-    
-
-    def scan_by_prefix(self, prefix: str) -> list[tuple[str, str]]:
-        """filtered + sorted"""
-        return [(k, v) for k, v in sorted(self._data.items(), key=lambda x:x[0]) if k.startswith(prefix) and not self._is_expired(k)]
